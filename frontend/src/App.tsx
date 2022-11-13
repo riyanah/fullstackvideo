fa77bf6a29834730a99a865f903b879cdb1bbd7f
Riyan Ahmed <riyan.ahmed@outlook.com>
2022-11-13 04:03:37 -0500

    post not working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, ChangeEvent, MouseEvent } from "react";
 import {
   ChakraProvider,
   Center,
@@ -19,20 +19,59 @@ import {
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
+type VideoProps = {
+  created_at: string;
+  id: number;
+  title: string;
+  updated_at: string;
+  video_url: string;
+};
+
 function App() {
   const [isSelected, setIsSelected] = useState<boolean>(false);
   const [selectedFile, setSelectedFile] = useState<File>();
-  const [allVideos, setAllVideos] = useState<File[]>([]);
+
+  const [allVideos, setAllVideos] = useState<VideoProps[]>([]);
+
   const [uploadSuccessful, setUploadSuccesful] = useState<boolean>(false);
   const [showSpinner, setShowSpinner] = useState(false);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/videos")
+    fetch("http://127.0.0.1:8000/videos/?format=json")
       .then((response) => response.json())
       .then((data) => {
         setAllVideos(data);
+        //console.log(allVideos);
       });
   }, [uploadSuccessful]);
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
+    }
+    setIsSelected(true);
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const onFileUpload = (e: MouseEvent<HTMLButtonElement>) => {
+    //console.log("Button Clicked");
+    setShowSpinner(true);
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append("file_attachment", selectedFile, selectedFile.name);
+
+      fetch("http://127.0.0.1:8000/videos/", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Success Posting");
+          setUploadSuccesful(!uploadSuccessful);
+          setShowSpinner(false);
+        });
+    }
+  };
   return (
     <ChakraProvider theme={theme}>
       <ColorModeSwitcher justifySelf="flex-end" />
@@ -41,24 +80,39 @@ function App() {
           <Heading>Your Video Gallery</Heading>
           <Text>Take a look at all your uploaded videos!!!</Text>
           <HStack>
-            <input type="file"></input>
-            {/* Need to add onChange and isDisabled */}
-            <Button size="lg" colorScheme="red" isDisabled={true}>
+            <input type="file" onChange={onInputChange}></input>
+
+            <Button
+              size="lg"
+              colorScheme="red"
+              isDisabled={!isSelected}
+              onClick={onFileUpload}
+            >
               Upload Video
             </Button>
+            {showSpinner && (
+              <Center>
+                <Spinner size="xl"></Spinner>
+              </Center>
+            )}
           </HStack>
           <Heading>Your Videos</Heading>
           <SimpleGrid columns={3} spacing={8}>
-            {allVideos.length !== 0 &&
-              allVideos.map((video) => {
-                <video
-                  src={video["video_url"]}
-                  autoPlay
-                  controls
-                  loop
-                  preload="auto"
-                ></video>;
-              })}
+            {allVideos?.map((value) => {
+              return (
+                <div>
+                  <div>{value.title}</div>
+                  <div>{value.created_at}</div>
+                  <video
+                    src={value["video_url"]}
+                    //  autoPlay
+                    controls
+                    loop
+                    preload="auto"
+                  ></video>
+                </div>
+              );
+            })}
           </SimpleGrid>
         </VStack>
       </Center>
--- fullstackvideo/fullstackvideo/settings.py
+++ fullstackvideo/fullstackvideo/settings.py
@@ -36,6 +36,14 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+CORS_ALLOW_CREDENTIALS = False
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework', 
     'uploader',
 ]
@@ -50,11 +59,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+   
 ]
 
 ROOT_URLCONF = 'fullstackvideo.urls'
